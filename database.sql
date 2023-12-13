6f8819edb22edeb1fb71a1a35f088600d28850c7
PUJASHINDU <shinduastawa2304@gmail.com>
2023-12-14 00:12:12 +0800

    first commit

---
+++ database.sql
@@ -0,0 +1,17 @@
+SELECT * FROM destination
+
+SELECT * FROM destination
+
+SELECT * FROM contact
+
+SELECT * FROM contacts
+
+SELECT * FROM contacts
+
+SELECT * FROM contacts
+
+
+SELECT * FROM contacts
+
+
+SELECT * FROM contacts
